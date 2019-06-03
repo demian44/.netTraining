bd786e09fbcd9b23075b41c11839faf58f0c8c19
demian44 <demi-44@hotmail.com>
2019-06-03 11:43:00 -0300

    Adding Autofac NuGets and configurate Global.asax in order to register all the controllers.

--- Code-first/CodeFirst/Controllers/EnrollmentsController.cs
+++ Code-first/CodeFirst/Controllers/EnrollmentsController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using CodeFirst.DAL;
 using CodeFirst.Models;
 
-namespace Code_first.Controllers
+namespace CodeFirst.Controllers
 {
     public class EnrollmentsController : Controller
     {
--- Code-first/CodeFirst/Global.asax.cs
+++ Code-first/CodeFirst/Global.asax.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using CodeFirst.Controllers;
 
 namespace CodeFirst
 {
@@ -12,6 +11,35 @@ namespace CodeFirst
     {
         protected void Application_Start()
         {
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers. (MvcApplication is the name of
+            // the class in Global.asax.)
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // ...or you can register individual controlllers manually.
+            builder.RegisterType<HomeController>().InstancePerRequest();
+            builder.RegisterType<StudentsController>().InstancePerRequest();
+            builder.RegisterType<EnrollmentsController>().InstancePerRequest();
+
+            // OPTIONAL: Register model binders that require DI.
+            builder.RegisterModelBinders(typeof(MvcApplication).Assembly);
+            builder.RegisterModelBinderProvider();
+
+            // OPTIONAL: Register web abstractions like HttpContextBase.
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            // OPTIONAL: Enable property injection in view pages.
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            // OPTIONAL: Enable property injection into action filters.
+            builder.RegisterFilterProvider();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
