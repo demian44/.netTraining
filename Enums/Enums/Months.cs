d13b9e2f90a4fcb066ecf12ad071918978b3754c
demian44 <demiandes@gmail.com>
2019-06-09 20:46:47 -0300

    Enum project - Code refactor

---
+++ Enums/Enums/Months.cs
@@ -0,0 +1,21 @@
+﻿namespace Enums
+{
+    public enum Months
+    {
+        // The enum values will be incremented from the number that you set in the first element
+        January = 1, // If you do not put any number it will start on 0
+        February,
+        March,
+        April,
+        May,
+        June,
+        //  If you set a number here, it will be incremented on one by one 
+        // from this point interrupting the previous counting. For example: July = 45
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+}
--- Enums/Enums/Program.cs
+++ Enums/Enums/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Enums
 {
@@ -19,7 +15,8 @@ namespace Enums
                $"{CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(1)}");
 
             Console.WriteLine($"Month in text format: {Months.January}");
-            var numberMonth = (int)Months.January;
+
+            int numberMonth = (int)Months.January;
             Console.WriteLine($"Month in numer format: {numberMonth}");
 
             // Here I will extract the names of all the enum elements 
@@ -36,24 +33,5 @@ namespace Enums
             // You only need play or google what you need
             Console.ReadKey();
         }
-    }
-
-    public enum Months
-    {
-        // The enum values will be incremented from the number that you set in the first element
-        January = 1, // If you do not put any number it will start on 0
-        February,
-        March,
-        April,
-        May,
-        June,
-        //  If you set a number here, it will be incremented on one by one 
-        // from this point interrupting the previous counting. For example: July = 45
-        July,
-        August,
-        September,
-        October,
-        November,
-        December
     }
 }
